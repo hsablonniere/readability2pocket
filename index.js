ad2794180ec1c5aa110cb1e986728f6b2a096124
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2014-02-16 12:33:56 +0100

    fix(index): update readability JSON parsing to new version

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 (function () {
+
+  // DOM elements
   var convertButton = document.getElementById('convert'),
-      readabilityTA = document.getElementById('readability-json'),
-      instapaperTA = document.getElementById('instapaper-html'),
-      before = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>Instapaper: Export</title></head><body><h1>Unread</h1><ol>',
-      between = '</ol><h1>Archive</h1><ol>',
-      after = '</ol></body></html>';
+    readabilityTA = document.getElementById('readability-json'),
+    instapaperTA = document.getElementById('instapaper-html');
 
+  // export templates
+  var before = '<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>Instapaper: Export</title></head><body><h1>Archive</h1><ol>',
+    between = '</ol><h1>Unread</h1><ol>',
+    after = '</ol></body></html>';
+
+  // on click
   convertButton.addEventListener('click', function () {
-    var readabilityLinks = JSON.parse(readabilityTA.value),
-        linkIdx,
-        link,
-        archivedItems = '',
-        unreadItems = '';
 
-    for (linkIdx in readabilityLinks) {
-      link = readabilityLinks[linkIdx];
-      if (link.archive) {
-        archivedItems += '<li><a href="' + link.article__url + '">' + link.article__title + '</a>: ' + link.article__excerpt + '</li>';
+    var items = JSON.parse(readabilityTA.value).bookmarks,
+      linkIdx,
+      item,
+      archivedItems = '',
+      unreadItems = '';
+
+    for (linkIdx = 0; linkIdx < items.length; linkIdx++) {
+      item = items[linkIdx];
+      if (item.archive) {
+        archivedItems += '<li><a href="' + item.article__url + '">' + item.article__title + '</a>: ' + item.article__excerpt + '</li>';
       } else {
-        unreadItems += '<li><a href="' + link.article__url + '">' + link.article__title + '</a>: ' + link.article__excerpt + '</li>';
+        unreadItems += '<li><a href="' + item.article__url + '">' + item.article__title + '</a>: ' + item.article__excerpt + '</li>';
       }
     }
 
-    instapaperTA.value = before + unreadItems + between + archivedItems + after;
+    instapaperTA.value = before + archivedItems + between + unreadItems + after;
   }, false);
 })();
